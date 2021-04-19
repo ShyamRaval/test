3f34469712c14a8a59e7f82fc0ede88cde2ad5fa
Shyam Raval <shyamraval131202@gmail.com>
2021-04-19 19:13:18 +0530

    complete without the checking thingie

--- main.cpp
+++ main.cpp
@@ -5,12 +5,8 @@ int main()
 	instance.game_status = 1;
 	while (instance.game_status)
 	{
-		while (instance.is_move_oku())
-		{
-			instance.status();
-			instance.get_input();
-			instance.process();
-		}
-		instance.move = 1;
+		instance.status();
+		instance.get_input();
+		instance.process();
 	}
 }
